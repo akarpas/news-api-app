846dd589acde5e0feb7b7ddcd5fb93f6a15007eb
Andreas Karpasitis <karpasitis@outlook.com>
2019-06-05 19:09:27 +0200

    chore: add components to main app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NewsComponent } from './news/news.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NewsComponent
   ],
   imports: [
     BrowserModule
